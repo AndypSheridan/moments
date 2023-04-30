e88feb46de62e8b173603778777642d85ac5f829
AndypSheridan <andy.p.sheridan@gmail.com>
2023-04-30 15:37:13 +0000

    Add post page and starter code

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -12,9 +12,14 @@ import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
+import { Alert, Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
+
+    const [errors, setErrors] = useState({});
+
     const [postData, setPostData] = useState({
         title: "",
         content: "",
@@ -30,6 +35,9 @@ function PostCreateForm() {
         });
     };
 
+    const imageInput = useRef(null);
+    const history = useHistory();
+
     const handleChangeImage = (event) => {
         if (event.target.files.length) {
             URL.revokeObjectURL(image);
@@ -40,7 +48,24 @@ function PostCreateForm() {
         }
     };
 
-    const [errors, setErrors] = useState({});
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+
+        formData.append("title", title);
+        formData.append("content", content);
+        formData.append("image", imageInput.current.files[0]);
+
+        try {
+            const { data } = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
+        } catch (err) {
+            console.log(err);
+            if (err.resonse?.status !== 401) {
+                setErrors(err.response?.data);
+            }
+        }
+    };
 
     const textFields = (
         <div className="text-center">
@@ -53,6 +78,11 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors.title?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
 
             <Form.Group controlId="content">
                 <Form.Label>Content</Form.Label>
@@ -64,10 +94,15 @@ function PostCreateForm() {
                     onChange={handleChange}
                 />
             </Form.Group>
+            {errors.content?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
 
             <Button
                 className={`${btnStyles.Button} ${btnStyles.Blue}`}
-                onClick={() => {}}
+                onClick={() => history.goBack()}
             >
                 cancel
             </Button>
@@ -81,7 +116,7 @@ function PostCreateForm() {
     );
 
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Row>
                 <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container
@@ -92,7 +127,7 @@ function PostCreateForm() {
                                 <>
                                     <figure>
                                         <Image
-                                            className="{appStyles}"
+                                            className={appStyles.Image}
                                             src={image}
                                             rounded
                                         />
@@ -121,8 +156,14 @@ function PostCreateForm() {
                                 id="image-upload"
                                 accept="image/*"
                                 onChange={handleChangeImage}
+                                ref={imageInput}
                             />
                         </Form.Group>
+                        {errors.image?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
                         <div className="d-md-none">{textFields}</div>
                     </Container>
                 </Col>
